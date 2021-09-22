dd6849f0640f4b3059007a2945b77fec498e5d7f
Abdullah Al Akash <aabdullahalakash@gmail.com>
2021-09-23 00:19:04 +0600

    Initial Level Complete and Startup UI

---
+++ src/components/Shop/Shop.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Shop.css'
+
+const Shop = () => {
+        return (
+                <div className="shop-container">
+                        <div className="product-container">
+                                <h2>This is Shop</h2>
+                        </div>
+                        <div className="cart-container">
+                                <h4>This is Cart Container</h4>
+                        </div>
+                </div>
+        );
+};
+
+export default Shop;
