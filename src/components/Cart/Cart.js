f8e4855e459911de4763e4c441d13fb9c1021140
Abdullah Al Akash <aabdullahalakash@gmail.com>
2021-09-23 12:58:45 +0600

    Cart Part Complete

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -8,9 +8,35 @@ const Cart = (props) => {
         for (const product of cart) {
                 total = total + product.price;
         }
+        // Shipping Cost:
+        let shippingCost = 0;
+        for (const product of cart) {
+                shippingCost = shippingCost + product.shipping;
+        }
+        // Tax Calculate:
+        const tax = total * .1;
+        // Final Total Calculate:
+        const finalTotal = total + shippingCost + tax;
         return (
                 <div>
-                        <p>Total: {total.toFixed(2)}</p>
+                        <div className="cart">
+                                <div className="flex">
+                                        <p className="text-left">Items Price:</p>
+                                        <p>${total.toFixed(2)}</p>
+                                </div>
+                                <div className="flex">
+                                        <p className="text-left">Shipping & Handling:</p>
+                                        <p>${shippingCost.toFixed(2)}</p>
+                                </div>
+                                <div className="flex">
+                                        <p className="text-left">10% Tax:</p>
+                                        <p>${tax.toFixed(2)}</p>
+                                </div>
+                        </div>
+                        <div className="flex">
+                                <h4>Total: </h4>
+                                <h4>{finalTotal.toFixed(2)}</h4>
+                        </div>
                 </div>
         );
 };
