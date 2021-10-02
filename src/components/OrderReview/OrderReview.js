c106e582e1fd13de67e98fb9a56be43bb0303c20
Abdullah Al Akash <aabdullahalakash@gmail.com>
2021-10-02 12:40:31 +0600

    Update useCart

--- src/components/OrderReview/OrderReview.js
+++ src/components/OrderReview/OrderReview.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import Cart from '../Cart/Cart';
+import useCart from '../hooks/useCart';
 import useProducts from '../hooks/useProducts';
 
 const OrderReview = () => {
         const [products] = useProducts();
+        const [cart] = useCart(products);
         return (
                 <div>
                         <h3>Total Products: {products.length}</h3>
+                        <h4>Cart Length: {cart.length}</h4>
                         <h2>This is Order Review</h2>
+                        <Cart cart={cart}></Cart>
                 </div>
         );
 };
